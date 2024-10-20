1c4a9200954095a008d928a77a06e04bacc64163
Nomenclaitor <qiyao43@gmail.com>
2024-10-20 13:13:24 +0200

    Nice initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { NextUIProvider } from '@nextui-org/react'
+import { AppProps } from "next/app";
+import '../styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+  )
 }
+
+export default MyApp
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [require("@nextui-org/react")],
+}
+
