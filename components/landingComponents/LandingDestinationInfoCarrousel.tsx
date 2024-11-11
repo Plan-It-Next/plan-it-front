461a15ab6062bd14278a35a9460cb6387c36e319
Nomenclaitor <qiyao43@gmail.com>
2024-11-11 20:01:53 +0100

    framer motion cubic bezier error fix

--- components/landingComponents/LandingDestinationInfoCarrousel.tsx
+++ components/landingComponents/LandingDestinationInfoCarrousel.tsx
@@ -29,12 +29,12 @@ interface Props {
 
 const fadeInTransition = {
     duration: 2,
-    ease: [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1]
+    ease: [0.4, 0, 0.2, 1]
 };
 
 const fadeOutTransition = {
     duration: 1,
-    ease: [1, 0.95, 0.9, 0.85, 0.8, 0.75, 0.7, 0.65, 0.6, 0.55, 0.5, 0.45, 0.4, 0.35, 0.3, 0.25, 0.2, 0.15, 0.1, 0.05, 0]
+    ease: [0.4, 1, 0.8, 1]
 };
 
 const DestinationCarousel: React.FC<Props> = ({ currentIndex, setCurrentIndex }) => {
