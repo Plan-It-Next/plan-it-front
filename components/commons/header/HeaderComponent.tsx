ee6a2747e51253889040cbce35baa9c234c6dfa5
Nomenclaitor <qiyao43@gmail.com>
2024-11-13 00:35:12 +0100

    components restructuring

--- components/commons/header/HeaderComponent.tsx
+++ components/commons/header/HeaderComponent.tsx
@@ -51,7 +51,7 @@ const Header: React.FC = () => {
         >
             <NavbarBrand>
                 <Link
-                    href="/"
+                    href="/public"
                     className={`font-bold text-inherit ${getTextColor()}`}
                 >
                     PlanIt
